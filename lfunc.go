9747689be17e6bbba5e3f1776c66c27f290bcbf6
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2022-02-02 12:58:54 -0700

    working lfunc with sqrt and avg

---
+++ lfunc.go
@@ -0,0 +1,167 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+)
+
+type Lfunc func(precision *big.Rat) (min, max *big.Rat)
+
+func Lint64(i int64) Lfunc {
+	return func(precision *big.Rat) (min, max *big.Rat) {
+		out := big.NewRat(i, 1)
+		return out, out
+	}
+}
+
+func Add(x, y Lfunc) Lfunc {
+	return func(precision *big.Rat) (min, max *big.Rat) {
+		min = new(big.Rat)
+		max = new(big.Rat)
+		xmin, xmax := x(precision)
+		ymin, ymax := y(precision)
+		min.Add(xmin, ymin)
+		max.Add(xmax, ymax)
+		return min, max
+	}
+}
+
+func Mul(x, y Lfunc) Lfunc {
+	return func(precision *big.Rat) (min, max *big.Rat) {
+		min = new(big.Rat)
+		max = new(big.Rat)
+		xmin, xmax := x(precision)
+		ymin, ymax := y(precision)
+		min.Mul(xmin, ymin)
+		max.Mul(xmax, ymax)
+		return min, max
+	}
+}
+
+func Quo(x, y Lfunc) Lfunc {
+	return func(precision *big.Rat) (min, max *big.Rat) {
+		min = new(big.Rat)
+		max = new(big.Rat)
+		xmin, xmax := x(precision)
+		ymin, ymax := y(precision)
+		min.Quo(xmin, ymin)
+		max.Quo(xmax, ymax)
+		return min, max
+	}
+}
+
+func Sub(x, y Lfunc) Lfunc {
+	return func(precision *big.Rat) (min, max *big.Rat) {
+		min = new(big.Rat)
+		max = new(big.Rat)
+		xmin, xmax := x(precision)
+		ymin, ymax := y(precision)
+		min.Sub(xmin, ymin)
+		max.Sub(xmax, ymax)
+		return min, max
+	}
+}
+
+func avg(a *big.Rat, vals ...*big.Rat) {
+	a.SetInt64(0)
+	for _, val := range vals {
+		a.Add(a, val)
+	}
+	count := big.NewRat(int64(len(vals)), 1)
+	a.Quo(a, count)
+}
+
+func Avg(vals ...Lfunc) Lfunc {
+	return func(precision *big.Rat) (min, max *big.Rat) {
+		min = new(big.Rat)
+		max = new(big.Rat)
+		mins := []*big.Rat{}
+		maxs := []*big.Rat{}
+		for _, val := range vals {
+			vmin, vmax := val(precision)
+			mins = append(mins, vmin)
+			maxs = append(maxs, vmax)
+		}
+		avg(min, mins...)
+		avg(max, maxs...)
+		return
+	}
+}
+
+func precisionCmp(min, max, precision *big.Rat) bool {
+	temp := new(big.Rat).Sub(max, min)
+	return temp.Cmp(precision) > 0
+}
+
+func sqrt(min, max, x *big.Rat, precision *big.Rat) {
+	min.SetInt64(1)
+	max.Set(x)
+	mid := big.NewRat(0,1)
+	minsq := new(big.Rat).Mul(min, min)
+	midsq := new(big.Rat).Mul(mid, mid)
+	maxsq := new(big.Rat).Mul(max, max)
+	for avg(mid, min, max); precisionCmp(minsq.Mul(min, min), maxsq.Mul(max, max), precision); avg(mid, min, max) {
+		if midsq.Mul(mid, mid).Cmp(x) < 0 {
+			min.Set(mid)
+		} else {
+			max.Set(mid)
+		}
+	}
+	return
+}
+
+func Sqrt(x Lfunc) Lfunc {
+	return func(precision *big.Rat) (*big.Rat, *big.Rat) {
+		minmin := new(big.Rat)
+		minmax := new(big.Rat)
+		maxmin := new(big.Rat)
+		maxmax := new(big.Rat)
+		xmin, xmax := x(precision)
+		sqrt(minmin, minmax, xmin, precision)
+		sqrt(maxmin, maxmax, xmax, precision)
+		return minmin, maxmax
+	}
+}
+
+func NewPrec(decplaces int64) *big.Rat {
+	out := big.NewRat(1,1)
+	tenth := big.NewRat(1,10)
+	var i int64
+	for i=0; i<decplaces; i++ {
+		out.Mul(out, tenth)
+	}
+	return out
+}
+
+func main() {
+	prprec := 300
+
+	x := Lint64(5)
+	y := Lint64(7)
+	z := Lint64(9)
+	xy := Add(x, y)
+	yz := Add(y, z)
+	xyyz := Add(xy, yz)
+	min, max := xyyz(big.NewRat(1,1))
+	fmt.Println(min, max)
+	fmt.Println(5+7+7+9)
+
+	fmt.Println(Avg(x, y, z)(big.NewRat(1,1)))
+
+	rt2 := Sqrt(Lint64(2))
+	smin, smax := rt2(big.NewRat(1,10000))
+	fmt.Println(smin.FloatString(prprec))
+	fmt.Println(smax.FloatString(prprec))
+
+	smin, smax = rt2(big.NewRat(1,1000000))
+	fmt.Println(smin.FloatString(prprec))
+	fmt.Println(smax.FloatString(prprec))
+
+	prec := NewPrec(200)
+	fmt.Println(prec)
+
+	r2p1 := Add(Sqrt(Lint64(2)), Lint64(1))
+	smin, smax = r2p1(prec)
+	fmt.Println(smin.FloatString(prprec))
+	fmt.Println(smax.FloatString(prprec))
+}
